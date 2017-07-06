8213a05f81790fe174599df90fff4fed5dbee3b2
HyoYeong <jung95510@gmail.com>
2017-07-06 18:01:21 +0900

    새소식 마크업 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,3 +1,4 @@
+// 서브메뉴 제어를 위한 스크립트 //
 $(function() {
     $(".main-menu > li").hover(
         function() {
@@ -13,25 +14,23 @@ $(function() {
     $(".main-menu ul li:last-child a").focusout(
         function() {
             $(this).parent().parent().css("display", "none");
-
         }
     );
-
-
 });
-// 서브메뉴 제어를 위한 스크립트 //
+
+// Tab 메뉴 제어를 위한 자바 스크립트
+$(function() {});
+$(".board h2").focus(function() {
+    $(this).parent().addClass("act").siblings().removeClass("act");
+});
+
 
 
 
 //* li의 지시자 this
-// $(function() {
-//$(".main-menu > li").hover(
-//   function() {},
-//    function() {}
-//);
-//$(".main-menu span").focusin(
-//    function() {}
-//);
+// $(); < wrapper 함수 
+//    function(argument) {act}
+// h2 보다는 class 이름을 주는게 더 좋다, 계층구조가 바뀔 수도 있기 때문이다.
 //$("main-menu ......").focusout(
 //function() {}
 //);
