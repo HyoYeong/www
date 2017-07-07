703be02697815f5dbf34b1d36292db8ec259b4bf
HyoYeong <jung95510@gmail.com>
2017-07-07 16:24:01 +0900

    인기사이트 디자인 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -16,14 +16,26 @@ $(function() {
             $(this).parent().parent().css("display", "none");
         }
     );
-});
 
-// Tab 메뉴 제어를 위한 자바 스크립트
-$(function() {});
-$(".board h2").focus(function() {
-    $(this).parent().addClass("act").siblings().removeClass("act");
-});
+    // Tab 메뉴 제어를 위한 자바 스크립트
+    $(function() {});
+    $(".board h2").focus(function() {
+        $(this).parent().addClass("act").siblings().removeClass("act");
 
+        //관련 사이트 키보드 포커스 시 클래스 추가 및 제거
+        $(".related-list a").focusin(function() {
+            $(this).parents(".related-list").addClass("list-act");
+
+        });
+        $(".related-list a").focusout(function() {
+            $(this).parents(".related-list").addClass("list-act");
+
+        });
+
+
+    });
+
+});
 
 
 
